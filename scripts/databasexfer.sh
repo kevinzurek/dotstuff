e85d2ed3cc551052467bb5d3a21ef162f8bc41ee
Kevin Zurek <kevinzurek@gmail.com>
2015-07-29 20:57:51 -0400

    add comments to databasexfer

--- scripts/databasexfer.sh
+++ scripts/databasexfer.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+#This script will help automatically deploy updates to fEMR's live research database.
+
 echo "Enter mysql username: "
 read username
 echo "Enter mysql password: "
@@ -11,6 +13,7 @@ read -e script
 #	read -s -p "Try another password?: " password
 #done	
 
+#this gives a warning for using a password on the command line
 mysql -u $username -p$password << TOKEN_FOR_ENDING_MYSQL_INPUT
 
 DROP DATABASE IF EXISTS femr_temp;
@@ -18,4 +21,5 @@ CREATE DATABASE femr_temp;
 
 TOKEN_FOR_ENDING_MYSQL_INPUT
 
+#so does this
 mysql -u $username -p$password femr_temp < $script
