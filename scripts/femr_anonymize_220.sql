04c070887733e1c3fa847290ed8059992ee14cb7
Kevin Zurek <kevinzurek@gmail.com>
2016-05-23 22:33:27 -0400

    do everything except change patient/patient encounter IDs for femr_anonymize_220

--- scripts/femr_anonymize_220.sql
+++ scripts/femr_anonymize_220.sql
@@ -39,7 +39,98 @@ last_name = concat("Anonmon", @incrementalUserNumber),
 `password` = "anon",
 notes = NULL;
 
+#Allow for the patient ID change to cascade to patient_encounters table
+ALTER TABLE `patient_encounters` 
+DROP FOREIGN KEY `fk_patient_encounter_patient_id_patients_id`;
+ALTER TABLE `patient_encounters` 
+ADD CONSTRAINT `fk_patient_encounter_patient_id_patients_id`
+  FOREIGN KEY (`patient_id`)
+  REFERENCES `patients` (`id`)
+  ON DELETE RESTRICT
+  ON UPDATE CASCADE;
+  
+#Change patient IDs
+#TBD
+
+
+#Revert patient_encounter table cascade setting change
+ALTER TABLE `patient_encounters` 
+DROP FOREIGN KEY `fk_patient_encounter_patient_id_patients_id`;
+ALTER TABLE `patient_encounters` 
+ADD CONSTRAINT `fk_patient_encounter_patient_id_patients_id`
+  FOREIGN KEY (`patient_id`)
+  REFERENCES `patients` (`id`)
+  ON DELETE RESTRICT
+  ON UPDATE RESTRICT;
+  
+  
+  
+  
+  
+  
+  
+  
+#Allow for the patient_encounter ID change to cascade to patient_encounter_vitals table
+ALTER TABLE `patient_encounter_vitals` 
+DROP FOREIGN KEY `fk_patient_encounter_vitals_patient_encounter_id`;
+ALTER TABLE `patient_encounter_vitals` 
+ADD CONSTRAINT `fk_patient_encounter_vitals_patient_encounter_id`
+  FOREIGN KEY (`patient_encounter_id`)
+  REFERENCES `patient_encounters` (`id`)
+  ON DELETE RESTRICT
+  ON UPDATE CASCADE;
+
+#Allow for the patient_encounter ID change to cascade to patient_prescriptions table
+ALTER TABLE `patient_prescriptions` 
+DROP FOREIGN KEY `fk_patient_prescriptions_encounter_id_patient_encounters_id`;
+ALTER TABLE `patient_prescriptions` 
+ADD CONSTRAINT `fk_patient_prescriptions_encounter_id_patient_encounters_id`
+  FOREIGN KEY (`encounter_id`)
+  REFERENCES `patient_encounters` (`id`)
+  ON DELETE RESTRICT
+  ON UPDATE CASCADE;
+  
+#Allow for the patient_encounter ID change to cascade to patient_encounter_photos table
+ALTER TABLE `patient_encounter_photos` 
+DROP FOREIGN KEY `patient_encounter_photos_ibfk_1`;
+ALTER TABLE `patient_encounter_photos` 
+ADD CONSTRAINT `patient_encounter_photos_ibfk_1`
+  FOREIGN KEY (`patient_encounter_id`)
+  REFERENCES `patient_encounters` (`id`)
+  ON DELETE RESTRICT
+  ON UPDATE CASCADE;
+
+
+#Change patient encounter IDs
+#TBD
 
 
 
-SET SQL_SAFE_UPDATES=0;
+
+#Revert patient_encounter_vitals table cascade setting change
+ALTER TABLE `patient_encounter_vitals` 
+DROP FOREIGN KEY `fk_patient_encounter_vitals_patient_encounter_id`;
+ALTER TABLE `patient_encounter_vitals` 
+ADD CONSTRAINT `fk_patient_encounter_vitals_patient_encounter_id`
+  FOREIGN KEY (`patient_encounter_id`)
+  REFERENCES `patient_encounters` (`id`)
+  ON UPDATE RESTRICT;
+
+#Revert patient_prescriptions table cascade setting change
+ALTER TABLE `patient_prescriptions` 
+DROP FOREIGN KEY `fk_patient_prescriptions_encounter_id_patient_encounters_id`;
+ALTER TABLE `patient_prescriptions` 
+ADD CONSTRAINT `fk_patient_prescriptions_encounter_id_patient_encounters_id`
+  FOREIGN KEY (`encounter_id`)
+  REFERENCES `patient_encounters` (`id`)
+  ON DELETE RESTRICT
+  ON UPDATE RESTRICT;
+  
+#Revert patient_encounter_photos table cascade setting change
+ALTER TABLE `patient_encounter_photos` 
+DROP FOREIGN KEY `patient_encounter_photos_ibfk_1`;
+ALTER TABLE `patient_encounter_photos` 
+ADD CONSTRAINT `patient_encounter_photos_ibfk_1`
+  FOREIGN KEY (`patient_encounter_id`)
+  REFERENCES `patient_encounters` (`id`)
+  ON UPDATE RESTRICT;
